b817741968db22191b8cdebe170a3e6e6f9d31e9
Giovanny <giov_w@hotmail.com>
2022-07-05 12:08:54 -0500

    Router Apis
    API
    http://localhost/api/users-registers
    http://localhost/api/users-registers/1

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DestinationController;
+use App\Http\Controllers\UserRegistersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('users-registers', [UserRegistersController::class, 'index']);
+
+Route::get("users-registers/{id}", [UserRegistersController::class, 'show',]);
+
+Route::get('destination', [DestinationController::class, 'index']);
